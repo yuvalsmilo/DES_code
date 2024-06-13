b0b976ecb9cb7ecae2dfe7a651a687c546202264
yuvalsmilo <36788569+yuvalsmilo@users.noreply.github.com>
2024-06-12 23:58:56 -0600

    Initial commit

---
+++ DES_test.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[63]:
+
+
+# Load components
+import numpy as np
+import matplotlib.pyplot as plt
+from landlab import RasterModelGrid
+from DE_Diffuser import DE_Diffuser
+
+
+# Domain parameters
+nrows = 30
+ncols = 3
+dx = 10
+
+grid = RasterModelGrid((int(nrows), int(ncols)), xy_spacing=int(dx))
+grid.set_closed_boundaries_at_grid_edges(True, True, True, False)
+topo = grid.add_zeros('topographic__elevation', at='node')
+de_diffuser = DE_Diffuser(grid)
+
+# main loop
+Ttot = 1000000
+
+t_clock = de_diffuser._t_clock
+while t_clock < Ttot:
+    de_diffuser.select_and_update_event()
+    de_diffuser.event_synchronization()
+    de_diffuser.event_scheduling()
+    t_clock = de_diffuser._t_clock
+    print('time = ', np.round(t_clock,1), ' -->  ', np.round(t_clock/Ttot,2), ' % out of Ttot')
+
+fig, ax = plt.subplots(2,1,
+                       figsize=(8,15))
+xvec = np.cumsum(np.ones_like(grid.at_node['topographic__elevation'][grid.nodes][:-1,1]) * grid.dx)
+for n in range(2):
+    ax[n].plot(xvec, grid.at_node['topographic__elevation'][grid.nodes][:-1,1], color = 'black', linewidth=3)
+    ax[n].set_xlabel('Distance [m]')
+    ax[n].set_ylabel('Elevation [m]')
+    if n ==1:
+        ax[n].set_aspect('equal', 'box')
+
+plt.show()
+
+
+
+grid = RasterModelGrid((int(nrows), int(ncols)), xy_spacing=int(dx))
+grid.set_closed_boundaries_at_grid_edges(True, True, True, False)
+topo = grid.add_zeros('topographic__elevation', at='node')
+hillslope__diffusivity = grid.add_ones('hillslope__diffusivity', at='node')
+hillslope__diffusivity *= 0.001
+start_low_d = 4
+end_low_d = 9
+hillslope__diffusivity[grid.nodes[start_low_d:end_low_d, 1]] *= 0.4
+de_diffuser = DE_Diffuser(grid,
+                          diffusivity =hillslope__diffusivity)
+
+Ttot = 10000000
+
+t_clock = de_diffuser._t_clock
+while t_clock < Ttot:
+    de_diffuser.select_and_update_event()
+    de_diffuser.event_synchronization()
+    de_diffuser.event_scheduling()
+    t_clock = de_diffuser._t_clock
+    print('time = ', np.round(t_clock,1), ' -->  ', np.round(t_clock/Ttot,2), ' % out of Ttot')
+
+fig, ax = plt.subplots(2,1,
+                       figsize=(8,15))
+xvec = np.cumsum(np.ones_like(grid.at_node['topographic__elevation'][grid.nodes][:-1,1]) * grid.dx)
+for n in range(2):
+    ax[n].plot(xvec, grid.at_node['topographic__elevation'][grid.nodes][:-1,1], color = 'black', linewidth=3)
+    ax[n].plot(xvec[start_low_d: end_low_d],grid.at_node['topographic__elevation'][grid.nodes][start_low_d:end_low_d,1], color = 'red', linewidth=3)
+    ax[n].set_xlabel('Distance [m]')
+    ax[n].set_ylabel('Elevation [m]')
+    if n ==1:
+        ax[n].set_aspect('equal', 'box')
+
+plt.show()
+
+
+# OLD  VERSION - BEFORE CREATING THE COMPONENT
+# flux_capacitor = grid.add_zeros('flux__capacitor', at='node')
+# delta_f_threhold = grid.add_zeros('delta_f__threhold', at='node')
+# sediment_flux = grid.add_zeros('sediment__flux', at='link')
+# R = grid.add_zeros('R', at='node')
+# Qlist = pd.DataFrame(columns=('node_id','dts','update_time'))
+#
+# # In[66]:
+#
+#
+# # Global parameters
+# uplift_rate = 0.001
+# diffusivity = 0.001
+# _ALPHA = 0.7
+# _CORE_NODES =  grid.core_nodes
+# _flatten_nodes = grid.nodes.flatten()
+# delta_f_max = 1 # delta_f_max > 0
+# w_lim = 0.5 # 0 < w_lim <=1
+# lamda_min = 1.1 # lambda_min >1
+# _Wcfl = 0.5 # 0 < _Wcfl <= 1
+# epsilon = 0.001
+# _non_active_link = grid.status_at_link==4
+# # In[67]:
+# t_clock = 0.0 # simulation elapsed time
+#
+#
+# # In[68]:
+# topo_grad_at_link = grid.calc_grad_at_link('topographic__elevation', out=None)
+# flux_at_link = diffusivity * topo_grad_at_link
+# outlinks_at_node = grid.link_at_node_is_downwind(topo_grad_at_link)
+#
+#
+# # In[69]:
+# topo_grad_at_link = grid.calc_grad_at_link('topographic__elevation', out=None)
+# CFL_prefactor = (
+#             _ALPHA  * grid.length_of_link[: grid.number_of_links] ** 2.0
+#         )
+# dt_at_links = np.abs(
+#     np.divide(CFL_prefactor,
+#               diffusivity,
+#              )
+#     )
+# cfl_dt_at_nodes  = grid.map_min_of_node_links_to_node(dt_at_links)
+# sorted_ids = np.argsort(cfl_dt_at_nodes)
+# sorted_ids = sorted_ids[np.isin(sorted_ids, grid.core_nodes)]
+# Qlist['node_id'] = _flatten_nodes[sorted_ids]
+# Qlist['t_p'] = np.zeros_like(cfl_dt_at_nodes[sorted_ids]) # dt at node
+# Qlist['next_update_time'] = t_clock + Qlist['t_p']
+# Qlist.set_index('node_id',inplace=True)
+# Qlist.sort_values(by=['next_update_time'],inplace=True)
+#
+# # In[77]:
+#
+#
+# ## Event processing
+# # Select the event with the smallest timestamp
+# def select_and_update_event(grid, Qlist, t_clock):
+#     node_id_at_work = Qlist.index[0]
+#     node_next_update_time  = Qlist.loc[node_id_at_work ,'next_update_time']
+#     tp = Qlist.loc[node_id_at_work ,'t_p']
+#
+#     # Update global time
+#     t_clock = node_next_update_time
+#
+#     fluxes_in, fluxes_out = calc_flux_at_node(grid, node_id_at_work)
+#
+#     R[node_id_at_work] = ((np.sum(fluxes_in) - np.sum(fluxes_out)) / grid.dx) + uplift_rate
+#
+#     # Execute event
+#     topo[node_id_at_work] += R[node_id_at_work] * (t_clock - tp)
+#     Qlist.loc[node_id_at_work, 't_p'] = t_clock
+#
+#     # Zero out flux capacitor
+#     flux_capacitor[node_id_at_work] = 0.0
+#
+#     return node_id_at_work, topo, flux_capacitor, node_next_update_time,t_clock,Qlist
+#
+# def event_synchronization(grid, node_id_at_work, Qlist, t_clock, parents = None):
+#
+#     uplift_rate = 0.01
+#     neighbors = grid.active_adjacent_nodes_at_node[node_id_at_work]
+#     flux_capacitor = grid.at_node['flux__capacitor']
+#     delta_f_threhold = grid.at_node['delta_f__threhold']
+#     node_next_update_time = Qlist.loc[node_id_at_work, 'next_update_time']
+#     if parents == None:
+#         parents  = []
+#     parents.append(node_id_at_work)
+#     for neighbor in neighbors:
+#         if grid.node_is_boundary(neighbor) == False and ~np.isin(neighbor, parents): # i.e., not a boundary
+#
+#             t_s = Qlist.loc[neighbor, 't_p']
+#
+#             # Calc out and in fluxes
+#             fluxes_in, fluxes_out = calc_flux_at_node(grid, neighbor)
+#
+#             R[neighbor] = ((np.sum(fluxes_in) - np.sum(fluxes_out)) / grid.dx) + uplift_rate
+#             delta_f = R[neighbor] * (t_clock - t_s )
+#             delta_f_cap = flux_capacitor[neighbor] + delta_f
+#             topo[neighbor] += delta_f
+#             t_s = t_clock
+#             Qlist.loc[neighbor, 't_p'] = t_s
+#             if np.abs(delta_f_cap) >= delta_f_threhold[neighbor]:
+#                 flux_capacitor[neighbor] = 0.0
+#                 event_synchronization(grid, neighbor,Qlist, t_clock,parents)
+#                 return
+#             else:
+#                 # link = find_common_link(grid, node_id_at_work, neighbor)
+#                 # topo_grad_at_link[link] = topo[grid.node_at_link_head[link]] - topo[grid.node_at_link_tail[link]]
+#                 # sediment_flux[link] = topo_grad_at_link[link] * grid.link_dirs_at_node[link] * diffusivity
+#                 #
+#                 # fluxes_out = sediment_flux[
+#                 #     grid.links_at_node[neighbor][np.greater(sediment_flux[grid.links_at_node[neighbor]],
+#                 #                                             0)]]
+#                 #
+#                 # fluxes_in = np.abs(sediment_flux[grid.links_at_node[neighbor][
+#                 #     np.greater(-sediment_flux[grid.links_at_node[neighbor]],
+#                 #                0)]])
+#                 fluxes_in, fluxes_out = calc_flux_at_node(grid, neighbor)
+#                 R[neighbor] = ((np.sum(fluxes_in) - np.sum(fluxes_out)) / grid.dx) + uplift_rate
+#
+#         if grid.node_is_boundary(neighbor) == True:
+#             if grid._node_status[neighbor] == 1:
+#                 fluxes_in, fluxes_out = calc_flux_at_node(grid, neighbor)
+#     return
+#     # In[56]:
+#
+#
+# def find_common_link(grid, node_a, node_b):
+#     try_tail = grid.node_at_link_tail[grid.links_at_node[node_a]] == node_b
+#     try_head = grid.node_at_link_head[grid.links_at_node[node_a]] == node_b
+#
+#     if np.any(try_tail):
+#         return grid.links_at_node[node_a][try_tail][0]
+#     elif np.any(try_head):
+#         return grid.links_at_node[node_a][try_head][0]
+#     else:
+#         return -1
+#
+#
+# ## Event scheduling
+# # Calc out and in fluxes
+# def event_scheduling(grid,node_id_at_work,t_clock):
+#
+#     # Calc out and in fluxes
+#     fluxes_in, fluxes_out = calc_flux_at_node(grid, node_id_at_work)
+#
+#
+#     R[node_id_at_work] = ((np.sum(fluxes_in) - np.sum(fluxes_out)) / grid.dx) + uplift_rate
+#
+#     delta_f_p_threshold = compute_local_target_increment(grid, node_id_at_work)
+#
+#     delta_tp = np.divide(delta_f_p_threshold, np.abs(R[node_id_at_work]))
+#
+#     #Qlist.loc[node_id_at_work, 't_p'] = delta_tp  # dt at node
+#     Qlist.loc[node_id_at_work, 'next_update_time'] = t_clock + delta_tp
+#     return Qlist
+#
+# def compute_local_target_increment(grid, node_id_at_work):
+#     neighbors = grid.active_adjacent_nodes_at_node[node_id_at_work]
+#     neighbors = neighbors[np.isin(neighbors, grid.core_nodes)]
+#     delta_f_cfl = np.abs(R[node_id_at_work]) * _Wcfl * cfl_dt_at_nodes[node_id_at_work]
+#     if delta_f_cfl < epsilon:
+#         delta_f_p_threshold = delta_f_cfl
+#     else:
+#         delta_f_p_threshold  = delta_f_cfl
+#         topo_neighbors_min = np.min(topo[neighbors])
+#         topo_neighbors_max = np.max(topo[neighbors])
+#
+#         lambda_t = np.min((np.divide(topo_neighbors_min, delta_f_p_threshold), lamda_min))
+#         if lambda_t > 1:
+#             delta_f_p_threshold = np.max((delta_f_p_threshold,np.min((np.divide(topo_neighbors_min,lambda_t),w_lim*(topo_neighbors_max-topo_neighbors_min)))))
+#
+#         delta_f_p_threshold = np.min((delta_f_max,delta_f_p_threshold))
+#
+#     return delta_f_p_threshold
+#
+#
+# def calc_flux_at_node(grid, node_id_at_work):
+#     # Calc out and in fluxes
+#
+#     topo_grad_at_link = grid.calc_grad_at_link('topographic__elevation', out=None)
+#     topo_grad_at_link[_non_active_link ] = 0.0 # take care for close boundaries
+#
+#     sediment_flux[grid.links_at_node[node_id_at_work]] = topo_grad_at_link[grid.links_at_node[node_id_at_work]] * \
+#                                                          grid.link_dirs_at_node[node_id_at_work] * diffusivity
+#
+#     fluxes_out = sediment_flux[
+#         grid.links_at_node[node_id_at_work][np.greater(sediment_flux[grid.links_at_node[node_id_at_work]],
+#                                                        0)]]
+#
+#     fluxes_in = np.abs(
+#         sediment_flux[
+#             grid.links_at_node[node_id_at_work][np.greater(-sediment_flux[grid.links_at_node[node_id_at_work]],
+#                                                            0)]])
+#     return fluxes_in, fluxes_out
+#
+#
+# ## main loope
+# Ttot = 500000
+# while t_clock < Ttot:
+#     Qlist.sort_values(by=['next_update_time'], inplace=True)
+#
+#     node_id_at_work, topo, flux_capacitor, node_next_update_time,t_clock,Qlist = select_and_update_event(grid, Qlist, t_clock)
+#
+#     event_synchronization(grid, node_id_at_work, Qlist, t_clock)
+#
+#     event_scheduling(grid, node_id_at_work,t_clock)
+#
+#
+# plt.plot(topo[grid.nodes][:,1]),plt.show()
+#
---
+++ DE_Diffuser.py
@@ -0,0 +1,351 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from landlab import RasterModelGrid
+import pandas as pd
+from landlab import Component
+
+
+
+class DE_Diffuser(Component):
+    _name = "DE_Diffuser"
+    _unit_agnostic = True
+
+
+    _info = {
+        "topographic__elevation": {
+            "dtype": float,
+            "intent": "inout",
+            "optional": True,
+            "units": "m",
+            "mapping": "node",
+            "doc": "Land surface topographic elevation",
+        },
+        "flux__capacitor": {
+            "dtype": float,
+            "intent": "out",
+            "optional": False,
+            "units": "-",
+            "mapping": "node",
+            "doc": "ADD--",
+        },
+        "sediment__flux": {
+            "dtype": float,
+            "intent": "out",
+            "optional": False,
+            "units": "-",
+            "mapping": "link",
+            "doc": "Sediment flux at link",
+        },
+        "delta_f__threshold": {
+            "dtype": float,
+            "intent": "out",
+            "optional": False,
+            "units": "-",
+            "mapping": "node",
+            "doc": "ADD",
+        },
+        "R": {
+            "dtype": float,
+            "intent": "out",
+            "optional": False,
+            "units": "-",
+            "mapping": "node",
+            "doc": "ADD",
+        },
+        "hillslope__diffusivity": {
+            "dtype": float,
+            "intent": "inout",
+            "optional": True,
+            "units": "-",
+            "mapping": "node",
+            "doc": "ADD",
+        },
+        "topographic__gradient": {
+            "dtype": float,
+            "intent": "out",
+            "optional": False,
+            "units": "-",
+            "mapping": "link",
+            "doc": "ADD",
+        },
+    }
+
+    def __init__(self,
+                 grid,
+                 uplift_rate=0.0001,
+                 diffusivity = 0.01,
+                 ALPHA = 0.7,
+                 delta_f_max = 1,   # delta_f_max > 0
+                 w_lim = 1,       # 0 < w_lim <=1
+                 lamda_min = 1.1,   # lambda_min > 1
+                 Wcfl = 0.5,        # 0 < _Wcfl <= 1
+                 epsilon = 0.001,
+                 t_clock = 0.0,     # simulation time
+                 ):
+
+        super(DE_Diffuser, self).__init__(grid)
+
+        if "topographic__elevation" not in grid.at_node:
+            grid.add_field("topographic__elevation",
+                           np.ones_like(self._grid.nodes.flatten()) * 0.0, at="node", dtype=float)
+
+
+        if "hillslope__diffusivity" not in grid.at_node:
+            grid.add_field("hillslope__diffusivity",
+                           np.ones_like(self._grid.nodes.flatten()) * diffusivity, at="node", dtype=float)
+
+        self.initialize_output_fields()
+        self._non_active_link = self._grid.status_at_link == 4
+        self._CORE_NODES = self._grid.core_nodes
+        self._flatten_nodes = self._grid.nodes.flatten()
+        self._ALPHA = ALPHA
+        self._delta_f_max = delta_f_max
+        self._lamda_min = lamda_min
+        self._t_clock = t_clock
+        self._uplift_rate = uplift_rate
+        self._Qlist = pd.DataFrame(columns=('node_id', 'dts', 'update_time'))
+        self._Wcfl = Wcfl
+        self._w_lim = w_lim
+        self._epsilon =epsilon
+
+        # Inititalization of cfl and Qlist
+        self._calc_cfl_dt()
+        self._init_Qlist()
+
+
+
+    def _calc_cfl_dt(self,):
+
+
+        diffusivity= self._grid.at_node["hillslope__diffusivity"]
+
+        CFL_prefactor = (
+                self._ALPHA * self._grid.length_of_link[: np.size(self._grid.nodes.flatten())] ** 2.0
+        )
+
+        self._cfl_dt_at_nodes  = np.abs(
+            np.divide(CFL_prefactor,
+                      diffusivity,
+                      )
+        )
+
+
+    def _init_Qlist(self):
+
+        sorted_ids = np.argsort(self._cfl_dt_at_nodes)
+        sorted_ids = sorted_ids[np.isin(sorted_ids, self._grid.core_nodes)]
+        self._Qlist['node_id'] = self._flatten_nodes[sorted_ids]
+        self._Qlist['t_p'] = np.zeros_like(self._cfl_dt_at_nodes[sorted_ids])  # dt at node
+        self._Qlist['next_update_time'] = self._t_clock
+        self._Qlist.set_index('node_id', inplace=True)
+        self._Qlist.sort_values(by=['next_update_time'], inplace=True)
+
+
+    def select_and_update_event(self):
+
+        # Pointers
+        Qlist = self._Qlist
+        grid = self._grid
+        uplift_rate =self._uplift_rate
+        R = self._grid.at_node['R']
+        topo = self._grid.at_node['topographic__elevation']
+        flux_capacitor = self._grid.at_node['flux__capacitor']
+
+        # Start
+        Qlist.sort_values(by=['next_update_time'], inplace=True)
+        self._node_id_at_work = Qlist.index[0]
+        node_id_at_work = self._node_id_at_work
+
+        node_next_update_time = Qlist.loc[node_id_at_work, 'next_update_time']
+        tp = Qlist.loc[node_id_at_work, 't_p']
+
+        # Update global time
+        self._t_clock = node_next_update_time
+
+        fluxes_in, fluxes_out = self._calc_flux_at_node(node_id_at_work = node_id_at_work)
+        R[node_id_at_work] = ((np.sum(fluxes_in) - np.sum(fluxes_out)) / grid.dx) + uplift_rate
+
+        # Execute event
+        topo[node_id_at_work] += R[node_id_at_work] * (self._t_clock - tp)
+        Qlist.loc[node_id_at_work, 't_p'] = self._t_clock
+
+        # Zero out flux capacitor
+        flux_capacitor[node_id_at_work] = 0.0
+
+
+    def event_synchronization(self, parent_node = None, parents=None):
+
+        if parent_node is None:
+            parent_node = self._node_id_at_work
+
+        node_id_at_work = parent_node
+        uplift_rate = self._uplift_rate
+        grid = self._grid
+        Qlist = self._Qlist
+        R = self._grid.at_node['R']
+        topo = self._grid.at_node['topographic__elevation']
+        neighbors = grid.active_adjacent_nodes_at_node[node_id_at_work]
+        flux_capacitor = grid.at_node['flux__capacitor']
+        delta_f_threhold = grid.at_node['delta_f__threshold']
+
+
+        if parents == None:
+            parents = []
+        parents.append(node_id_at_work)
+
+
+        for neighbor in neighbors:
+            if grid.node_is_boundary(neighbor) == False and ~np.isin(neighbor, parents):  # i.e., not a boundary and not a parent
+
+                t_s = Qlist.loc[neighbor, 't_p']
+
+                # Calc out and in fluxes
+                fluxes_in, fluxes_out = self._calc_flux_at_node(node_id_at_work = neighbor)
+
+                R[neighbor] = ((np.sum(fluxes_in) - np.sum(fluxes_out)) / grid.dx) + uplift_rate
+                delta_f = R[neighbor] * (self._t_clock - t_s)
+                delta_f_cap = flux_capacitor[neighbor] + delta_f
+                topo[neighbor] += delta_f
+                t_s = self._t_clock
+                Qlist.loc[neighbor, 't_p'] = t_s
+                if np.abs(delta_f_cap) >= delta_f_threhold[neighbor]:
+                    flux_capacitor[neighbor] = 0.0
+                    #self.event_synchronization(parent_node = neighbor, parents = parents)
+                    #parents  = None
+                    self.run_one_event(parent = neighbor, parents=parents)
+                    return
+                else:
+
+                    self._correct_flux_between_nodes(node_s=neighbor, node_p=node_id_at_work)
+
+            if grid.node_is_boundary(neighbor) == True:
+                if grid._node_status[neighbor] == 1:
+                    self._correct_flux_between_nodes(node_s = neighbor, node_p = node_id_at_work)
+        return
+
+    def event_scheduling(self, parent = None):
+
+        if parent == None:
+            parent = self._node_id_at_work
+
+        node_id_at_work = parent
+        uplift_rate = self._uplift_rate
+        grid = self._grid
+        Qlist = self._Qlist
+        R = self._grid.at_node['R']
+
+        # Calc out and in fluxes
+        fluxes_in, fluxes_out = self._calc_flux_at_node(node_id_at_work)
+
+        R[node_id_at_work] = ((np.sum(fluxes_in) - np.sum(fluxes_out)) / grid.dx) + uplift_rate
+
+        delta_f_p_threshold = self._compute_local_target_increment(node_id_at_work)
+
+        if delta_f_p_threshold == np.inf:
+            Qlist.loc[node_id_at_work, 'next_update_time'] = np.inf
+            return
+        delta_tp = np.divide(delta_f_p_threshold, np.abs(R[node_id_at_work]))
+
+        Qlist.loc[node_id_at_work, 'next_update_time'] = self._t_clock + delta_tp
+
+    def _compute_local_target_increment(self, node_id_at_work):
+
+        grid = self._grid
+        R = self._grid.at_node['R']
+        _Wcfl = self._Wcfl
+        epsilon = self._epsilon
+        topo = self._grid.at_node['topographic__elevation']
+        cfl_dt_at_nodes = self._cfl_dt_at_nodes
+        w_lim = self._w_lim
+        lamda_min = self._lamda_min
+        delta_f_max = self._delta_f_max
+
+        neighbors = grid.active_adjacent_nodes_at_node[node_id_at_work]
+        neighbors = neighbors[np.isin(neighbors, grid.core_nodes)]
+        delta_f_cfl = np.abs(R[node_id_at_work]) * _Wcfl * cfl_dt_at_nodes[node_id_at_work]
+        if delta_f_cfl < epsilon:
+            return np.inf
+        else:
+            delta_f_p_threshold = np.copy(delta_f_cfl)
+            topo_neighbors_min = np.min(topo[neighbors])
+            topo_neighbors_max = np.max(topo[neighbors])
+
+            lambda_t = np.min((np.divide(topo_neighbors_min, delta_f_p_threshold), lamda_min))
+            if lambda_t > 1:
+                delta_f_p_threshold = np.max((delta_f_p_threshold, np.min(
+                    (np.divide(topo_neighbors_min, lambda_t), w_lim * (topo_neighbors_max - topo_neighbors_min)))))
+
+            delta_f_p_threshold = np.min((delta_f_max, delta_f_p_threshold))
+
+        return delta_f_p_threshold
+
+    def _calc_flux_at_node(self, node_id_at_work):
+        # Calc out and in fluxes
+        grid = self._grid
+        _non_active_link = self._non_active_link
+        topo_grad_at_link = self._grid.at_link['topographic__gradient']
+        sediment_flux = self.grid.at_link['sediment__flux']
+        diffusivity_at_link = grid.map_node_to_link_linear_upwind(grid.at_node['hillslope__diffusivity'],topo_grad_at_link)
+
+        topo_grad_at_link[:] = grid.calc_grad_at_link('topographic__elevation', out=None)
+
+        sediment_flux[grid.links_at_node[node_id_at_work]] = topo_grad_at_link[grid.links_at_node[node_id_at_work]] * (grid.link_dirs_at_node[node_id_at_work] *
+                                                               diffusivity_at_link[grid.links_at_node[node_id_at_work]])
+
+        sediment_flux[_non_active_link] = 0.0  # take care for close boundaries
+        fluxes_out = sediment_flux[
+            grid.links_at_node[node_id_at_work][np.greater(sediment_flux[grid.links_at_node[node_id_at_work]],
+                                                           0)]]
+
+        fluxes_in = np.abs(
+            sediment_flux[
+                grid.links_at_node[node_id_at_work][np.greater(-sediment_flux[grid.links_at_node[node_id_at_work]],
+                                                               0)]])
+        return fluxes_in, fluxes_out
+
+    def run_one_event(self, parent, parents):
+
+        self.event_synchronization(parent_node=parent, parents=parents)
+        self.event_scheduling(parent=parent)
+
+
+
+    def _correct_flux_between_nodes(self, node_s, node_p):
+
+        grid = self._grid
+        topo = self._grid.at_node['topographic__elevation']
+        gtopo = self._grid.at_link['topographic__gradient']
+        sediment_flux = self._grid.at_link['sediment__flux']
+        uplift_rate = self._uplift_rate
+        R = self.grid.at_node['R']
+
+
+        try_tail = grid.node_at_link_tail[grid.links_at_node[node_s]] == node_p
+        try_head = grid.node_at_link_head[grid.links_at_node[node_s]] == node_p
+
+        if np.any(try_tail):
+            link =  grid.links_at_node[node_s][try_tail][0]
+        elif np.any(try_head):
+            link =  grid.links_at_node[node_s][try_head][0]
+        else:
+            link = -1
+
+        gtopo[link] = (topo[grid.node_at_link_head[link]] - topo[grid.node_at_link_tail[link]]) / grid.dx
+        diffusivity_at_link = grid.map_node_to_link_linear_upwind(grid.at_node['hillslope__diffusivity'],
+                                                                  gtopo)
+        sediment_flux[grid.links_at_node[node_s]] = gtopo[grid.links_at_node[node_s]] * grid.link_dirs_at_node[node_s] * diffusivity_at_link[grid.links_at_node[node_s]]
+        sediment_flux[self._non_active_link] = 0.0  # take care for close boundaries
+        fluxes_out = sediment_flux[
+            grid.links_at_node[node_s][
+                np.greater(sediment_flux[grid.links_at_node[node_s]],
+                           0)]]
+
+        fluxes_in = np.abs(
+            sediment_flux[
+                grid.links_at_node[node_s][
+                    np.greater(-sediment_flux[grid.links_at_node[node_s]],
+                               0)]])
+        R[node_s] = ((np.sum(fluxes_in) - np.sum(fluxes_out)) / grid.dx) + uplift_rate
+
+
+
